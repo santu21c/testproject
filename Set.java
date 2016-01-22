2d3251f3c1b0c436145140eccf3739cf377e606b
santoshreddy chiliveru <santoshreddy chiliveru>
2016-01-21 20:28:49 -0500

    hii second file

---
+++ Set.java
@@ -0,0 +1,32 @@
+package dog;
+
+import java.sql.Connection;
+
+public class Set {
+//	San san;
+	String message;
+	//Connection con = null;
+	
+	 
+	 
+
+	public String getMessage() {
+   System.out.println("message is" +message);
+   return message;
+	}
+
+//	public San getSan() {
+	//	return san;
+	//}
+
+	//public void setSan(San san) {
+		//this.san = san;
+	//}
+
+	public void setMessage(String message) {
+		
+		this.message = message;
+	}
+	
+
+}
